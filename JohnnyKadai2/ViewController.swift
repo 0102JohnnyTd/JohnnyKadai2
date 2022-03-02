3644bf22aca842d035d89796e8aa72b8f2d74bcf
0102johnny <rt2824.recruit.1022@gmail.com>
2022-03-03 07:53:48 +0900

    UIの作成

--- JohnnyKadai2/ViewController.swift
+++ JohnnyKadai2/ViewController.swift
@@ -8,6 +8,17 @@
 import UIKit
 
 final class ViewController: UIViewController {
+    @IBOutlet private weak  var firstValueTextField: UITextField!
+
+    @IBOutlet private weak  var secondValueTextField: UITextField!
+
+    @IBOutlet private weak  var segmentedControl: UISegmentedControl!
+
+    @IBAction private func caluclateButton(_ sender: Any) {
+    }
+    
+    @IBOutlet private weak  var caluculationResultLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
