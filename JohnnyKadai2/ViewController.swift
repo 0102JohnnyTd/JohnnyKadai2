7528dc139ab37ec5159c79efaee955feeed5d382
0102johnny <rt2824.recruit.1022@gmail.com>
2022-03-04 08:01:15 +0900

    segmentedControlの処理のコードが冗長だったので修正

--- JohnnyKadai2/ViewController.swift
+++ JohnnyKadai2/ViewController.swift
@@ -15,59 +15,29 @@ final class ViewController: UIViewController {
 
     @IBOutlet private weak var segmentedControl: UISegmentedControl!
 
-
     @IBAction private func valueChanged(_ sender: Any) {
     }
 
     @IBAction private func caluclateButton(_ sender: Any) {
+        let firstValue = firstValueTextField.textToInt
+        let secondValue = secondValueTextField.textToInt
+
         if segmentedControl.selectedSegmentIndex == 0 {
-            addition()
+            calculationResultLabel.text =  String(firstValue + secondValue)
         } else if segmentedControl.selectedSegmentIndex == 1 {
-            subtraction()
+            calculationResultLabel.text = String(firstValue - secondValue)
         } else if segmentedControl.selectedSegmentIndex == 2 {
-            multiplication()
+            calculationResultLabel.text = String(firstValue * secondValue)
         } else {
-            division()
+            if secondValue == 0 {
+                calculationResultLabel.text = "割る数には0以外を入力してください"
+            } else {
+                calculationResultLabel.text = String(firstValue / secondValue)
+            }
         }
     }
 
-    private func addition() {
-       let firstValue = firstValueTextField.textToInt
-       let secondValue = secondValueTextField.textToInt
-
-        calculationResultLabel.text =  String(firstValue + secondValue)
-    }
-
-    private func subtraction() {
-        let firstValue = firstValueTextField.textToInt
-        let secondValue = secondValueTextField.textToInt
-
-       calculationResultLabel.text = String(firstValue - secondValue)
-    }
-
-    private func multiplication() {
-        let firstValue = firstValueTextField.textToInt
-        let secondValue = secondValueTextField.textToInt
-
-       calculationResultLabel.text = String(firstValue * secondValue)
-    }
-
-    private func division() {
-        let firstValue = firstValueTextField.textToInt
-        let secondValue = secondValueTextField.textToInt
-
-        if secondValue == 0 {
-            calculationResultLabel.text = "割る数には0以外を入力してください"
-        } else {
-            calculationResultLabel.text = String(firstValue / secondValue)
-        }
-    }
-    
     @IBOutlet private weak var calculationResultLabel: UILabel!
 }
 
-extension UITextField {
-    var textToInt: Int {
-        text.flatMap { Int($0) } ?? 0
-    }
-}
+
