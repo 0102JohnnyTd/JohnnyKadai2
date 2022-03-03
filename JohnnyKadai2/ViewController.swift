d63c951e4ba5a208d538abe3d974f051c446f791
0102johnny <rt2824.recruit.1022@gmail.com>
2022-03-03 17:39:09 +0900

    textFieldの入力値をInt型に変換する処理を追加

--- JohnnyKadai2/ViewController.swift
+++ JohnnyKadai2/ViewController.swift
@@ -8,17 +8,22 @@
 import UIKit
 
 final class ViewController: UIViewController {
-    @IBOutlet private weak  var firstValueTextField: UITextField!
+    @IBOutlet private weak var firstValueTextField: UITextField!
 
-    @IBOutlet private weak  var secondValueTextField: UITextField!
+    @IBOutlet private weak var secondValueTextField: UITextField!
 
-    @IBOutlet private weak  var segmentedControl: UISegmentedControl!
+    @IBOutlet private weak var segmentedControl: UISegmentedControl!
 
     @IBAction private func caluclateButton(_ sender: Any) {
+        let textFieldArray: [UITextField] = [firstValueTextField,secondValueTextField]
+
+        let result = textFieldArray
+            .map { $0.text ?? "" }
+            .map { Int($0) ?? 0 }
     }
     
-    @IBOutlet private weak  var caluculationResultLabel: UILabel!
-    
+    @IBOutlet private weak var caluculationResultLabel: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
