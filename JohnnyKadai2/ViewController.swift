62a69b62fc8995db8ed4170c9bfd6139c3475bda
0102johnny <rt2824.recruit.1022@gmail.com>
2022-03-04 07:42:26 +0900

    Segmented Controlと処理の追加

--- JohnnyKadai2/ViewController.swift
+++ JohnnyKadai2/ViewController.swift
@@ -8,28 +8,73 @@
 import UIKit
 
 final class ViewController: UIViewController {
+
     @IBOutlet private weak var firstValueTextField: UITextField!
 
     @IBOutlet private weak var secondValueTextField: UITextField!
 
-    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    @IBOutlet private weak var segmentedControl: UISegmentedControl!
 
-    
-    @IBAction func valueChanged(_ sender: Any) {
+
+    @IBAction private func valueChanged(_ sender: Any) {
     }
-    
+
     @IBAction private func caluclateButton(_ sender: Any) {
-        let textFieldArray: [UITextField] = [firstValueTextField,secondValueTextField]
-
-        let result = textFieldArray
-                .map { $0.text ?? "" }
-            .map { Int($0) ?? 0 }
+        if segmentedControl.selectedSegmentIndex == 0 {
+            addition()
+        } else if segmentedControl.selectedSegmentIndex == 1 {
+            subtraction()
+        } else if segmentedControl.selectedSegmentIndex == 2 {
+            multiplication()
+        } else {
+            division()
+        }
     }
+
+    private func addition() {
+       let firstValue = firstValueTextField.textToInt
+       let secondValue = secondValueTextField.textToInt
+
+        calculationResultLabel.text =  String(firstValue + secondValue)
+    }
+
+    private func subtraction() {
+        let firstValue = firstValueTextField.textToInt
+        let secondValue = secondValueTextField.textToInt
+
+       calculationResultLabel.text = String(firstValue - secondValue)
+    }
+
+    private func multiplication() {
+        let firstValue = firstValueTextField.textToInt
+        let secondValue = secondValueTextField.textToInt
+
+       calculationResultLabel.text = String(firstValue * secondValue)
+    }
+
+    private func division() {
+        let firstValue = firstValueTextField.textToInt
+        let secondValue = secondValueTextField.textToInt
+
+        if secondValue == 0 {
+            calculationResultLabel.text = "割る数には0以外を入力してください"
+        } else {
+            calculationResultLabel.text = String(firstValue / secondValue)
+        }
+    }
+
+
     
-    @IBOutlet private weak var caluculationResultLabel: UILabel!
+    @IBOutlet private weak var calculationResultLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+}
+
+extension UITextField {
+    var textToInt: Int {
+        text.flatMap { Int($0) } ?? 0
     }
 }
