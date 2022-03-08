66253d2a6106c1b0e2610ee088e157f8e25ac6b8
0102johnny <rt2824.recruit.1022@gmail.com>
2022-03-08 10:36:17 +0900

    フィードバック後、内容を修正

--- JohnnyKadai2/Main/Controller/ViewController.swift
+++ JohnnyKadai2/Main/Controller/ViewController.swift
@@ -18,19 +18,26 @@ final class ViewController: UIViewController {
         let firstValue = firstValueTextField.textToFloat
         let secondValue = secondValueTextField.textToFloat
 
-        guard segmentedControl.selectedSegmentIndex != 0 else {
-            return calculationResultLabel.text = FetchCalculations.fetchCalculationResult(typeOfCalculation: .addition, firstValue, secondValue)
+        // FetchCalculations構造体の列挙型を型として指定する
+        // Switch文のcaseに応じてenumが持つcaseを指定することができる
+        let typeOfCalculation: FetchCalculations.TypeOfCalculation
+
+        // guard文ではなくswitch文で条件分岐する
+        switch segmentedControl.selectedSegmentIndex {
+        case 0:
+            typeOfCalculation = .addition
+        case 1:
+            typeOfCalculation = .subtraction
+        case 2:
+            typeOfCalculation = .multiplication
+        case 3:
+            typeOfCalculation = .division
+        default:
+            fatalError("エラーが発生しました")
         }
-        guard segmentedControl.selectedSegmentIndex != 1 else {
-            return calculationResultLabel.text = FetchCalculations.fetchCalculationResult(typeOfCalculation: .subtraction, firstValue, secondValue)
-        }
-        guard segmentedControl.selectedSegmentIndex != 2 else {
-            return  calculationResultLabel.text = FetchCalculations.fetchCalculationResult(typeOfCalculation: .multiplication, firstValue, secondValue)
-        }
-        guard segmentedControl.selectedSegmentIndex != 3 else {
-            return calculationResultLabel.text = FetchCalculations.fetchCalculationResult(typeOfCalculation: .division, firstValue, secondValue)
-        }
-        return
+        // 引数の値のtypeOfCalculationには上記のswitchに該当するtypeが入る。
+        // よってfetchCalculationResult()の呼び出しが一度で済む
+        calculationResultLabel.text = FetchCalculations.fetchCalculationResult(typeOfCalculation: typeOfCalculation, firstValue, secondValue)
     }
 
     @IBOutlet private weak var calculationResultLabel: UILabel!
