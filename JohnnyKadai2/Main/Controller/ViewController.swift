8ca7c771f1fabd1d93b1ad98dd3a70dae9ab95e0
0102johnny <rt2824.recruit.1022@gmail.com>
2022-03-09 15:37:29 +0900

    switch文で条件分岐していたsegmentedControl.selectedSegmentIndexの処理をguard文でコンパクトに設定

--- JohnnyKadai2/Main/Controller/ViewController.swift
+++ JohnnyKadai2/Main/Controller/ViewController.swift
@@ -18,24 +18,10 @@ final class ViewController: UIViewController {
         let firstValue = firstValueTextField.textToFloat
         let secondValue = secondValueTextField.textToFloat
 
-        // FetchCalculations構造体の列挙型を型として指定する
-        // Switch文のcaseに応じてenumが持つcaseを指定することができる
-        let typeOfCalculation: FetchCalculations.TypeOfCalculation
-
-        // guard文ではなくswitch文で条件分岐する
-        switch segmentedControl.selectedSegmentIndex {
-        case 0:
-            typeOfCalculation = .addition
-        case 1:
-            typeOfCalculation = .subtraction
-        case 2:
-            typeOfCalculation = .multiplication
-        case 3:
-            typeOfCalculation = .division
-        default:
+        guard let typeOfCalculation = FetchCalculations.TypeOfCalculation(rawValue: segmentedControl.selectedSegmentIndex) else {
             fatalError("エラーが発生しました")
         }
-        // 引数の値のtypeOfCalculationには上記のswitchに該当するtypeが入る。
+        // 引数の値のtypeOfCalculationには上記のsegmentedControlに該当するtypeが入る。
         // よってfetchCalculationResult()の呼び出しが一度で済む
         calculationResultLabel.text = FetchCalculations.fetchCalculationResult(typeOfCalculation: typeOfCalculation, firstValue, secondValue)
     }
